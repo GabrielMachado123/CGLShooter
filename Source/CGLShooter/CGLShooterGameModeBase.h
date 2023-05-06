75d2e703fe3c11d9d42b0fe8b255b15221f09a4d
nufv12 <nufv12@gmail.com>
2023-05-06 18:51:39 +0200

    Initial commit

---
+++ Source/CGLShooter/CGLShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CGLShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CGLSHOOTER_API ACGLShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
